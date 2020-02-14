60ce36a4d98a3bec5da8df69cce864757e6eb0ce
Osiel <ocl.vargasanchez@gmail.com>
2020-02-13 21:29:28 -0600

    set up mongodb

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+mongoose.connect(process.env.mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(logger('dev'));
 app.use(express.json());
